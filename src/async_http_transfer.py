e0d498db832c8a6f0c7ac01b9eb3a9fbae3ba756
Amanda Ibsen <amanda.ibsen@gmail.com>
2024-02-22 16:17:23 +0000

    test to bring .fits files through http asynchronouly

---
+++ src/async_http_transfer.py
@@ -0,0 +1,85 @@
+import aiohttp
+import asyncio
+import os
+import sys
+from urllib.parse import urlparse
+import aiofiles 
+import cProfile
+import pandas as pd
+
+
+async def fetch(url, session):
+    async with session.get(url) as response:
+        return await response.read()
+
+async def write_fits_file(file_path, data):
+    async with aiofiles.open(file_path, 'wb') as f:
+        await f.write(data)
+    # with fits.open(file_path, mode='wb') as hdul:
+    #     hdul.append(fits.ImageHDU(data=data))
+    #     hdul.writeto(file_path, overwrite=True)
+
+async def download_and_write(url, butler_directory, session):
+    parsed_url = urlparse(url)
+    url_path = parsed_url.path
+
+    directory, filename = os.path.split(url_path)
+    local_directory = os.path.join(butler_directory, directory[1:])
+    file_path = os.path.join(local_directory, filename)
+
+    #create directories
+    os.makedirs(local_directory, exist_ok=True)
+
+    #fetch data
+    data = await fetch(url, session)
+    #write 
+    await write_fits_file(file_path,data)
+
+async def main(urls, butler_directory, max_connections=4):
+
+    connector = aiohttp.TCPConnector(limit=max_connections)
+    async with aiohttp.ClientSession(connector=connector) as session:
+        tasks = [download_and_write(url, butler_directory,session) for url in urls]
+    await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+
+    len_argv = len(sys.argv)
+    if len_argv < 2:
+        print("Usage: python async_http_transfer.py file_with_urls.csv")
+        sys.exit(1)
+
+    url_file = sys.argv[1]
+    max_connections = 4 if len_argv <= 2 else sys.argv[2]
+    butler_directory = '/data/butler/dp02/' if len_argv <= 3 else sys.argv[3]
+    
+    url_file_path = os.path.join(
+        os.path.dirname(
+            os.path.abspath(__file__)),
+        '/csv_files/',
+        url_file)
+    
+    # pd.set_option('display.expand_frame_repr', False)
+    pd.set_option('max_colwidth', 100)
+    
+    try:
+        urls = pd.read_csv(url_file, names=['urls'])
+        urls = urls.head()
+        url_list = urls.urls.tolist()
+        
+        profiler = cProfile.Profile()
+        profiler.enable()
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main(url_list, butler_directory, max_connections))
+        
+        profiler.disable()
+        profiler_file = "profiler_stats.prof"
+        profiler.dump_stats(profiler_file)
+
+        # print(f"Profiler statistics saved to {profiler_file}")
+
+    except FileNotFoundError:
+        print("the file "+str(url_file)+" does not exist.")
+    except Exception as e:
+        print("An exception occurred:", str(e))
