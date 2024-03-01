4c6a4023c6d17ffb368e18f5ee2d2188688054e9
Amanda Ibsen <amanda.ibsen@gmail.com>
2024-03-01 15:15:48 +0000

    script to do ingestion and prepare files for ingestion

--- src/ingest.py
+++ src/ingest.py
@@ -2,18 +2,18 @@ import os
 import subprocess
 
 # Directory path
-directory = '~/dp02_in2p3_to_somerville/ecsv'
+directory = 'ecsv'
 
 
 # Iterate over each file in the directory
 for filename in os.listdir(directory):
-    run = filename.split('_')[-1].split['.'][0]
+    run = filename.split('_')[-1].split('.')[0]
     print(run)
     # Check if the path is a file (not a directory)
     filepath = os.path.join(directory, filename)
     if os.path.isfile(filepath):
         # Construct the Unix command with the filepath
-        command_to_run = f"butler ingest-files . deepCoadd_calexp 2.2i/runs/DP0.2/{run} {filepath}"
+        command_to_run = f"butler ingest-files /data/butler/dp02 deepCoadd_calexp 2.2i/runs/DP0.2/{run} {filepath}"
         
         # Execute the Unix command using subprocess
         subprocess.run(command_to_run, shell=True)
